a544b2afd6f970c1b0f78942c72534d119cb1431
Jan <j.bolting@gmx.de>
2015-08-02 19:53:10 +0200

    Getting closer to displaying data received from server as json file with knockout.js.

---
+++ public_html/back/front/app/scripts/app.js
@@ -0,0 +1,35 @@
+var isauthenticated = true;
+var user = "bob";
+
+function userisauthenticated(number) {
+    return isauthenticated;
+}
+
+function ViewModel() {
+    var self = this;
+    // Data model:
+    self.mainpagelayout = new MainPageViewModel();
+    self.userpage = new UserPageViewModel();
+}
+
+function UserPageViewModel() {
+    var self = this;
+    // Data model:
+    // self.events = ko.observableArray([]);
+    // self.name = ko.observable("Jim-Bob");
+    // Load user data from server and populate the view model:
+    $.getJSON("/userdata/"+user, function (receivedData) {
+        self.user = ko.mapping.fromJS(receivedData);
+        console.log(self.user.name());
+    });
+}
+
+// View model for page layout:
+function MainPageViewModel() {
+    var self = this;
+    // Data model:
+    self.signinPageVisible = ko.observable(false);
+    self.userPageVisible = ko.observable(true);    
+}
+
+// ko.applyBindings(new ViewModel());
