4c1ace6acdfd30fd0a1fc7eeb59c7ac452937ecb
Jan <j.bolting@gmx.de>
2015-08-06 20:26:42 +0200

    Added functionality to
    - add new exercise types
    - remove exercises

--- public_html/back/front/app/scripts/app.js
+++ public_html/back/front/app/scripts/app.js
@@ -24,21 +24,21 @@ function UserPageViewModel() {
     var self = this;
     // Create and populate the view model:
     self.user = ko.mapping.fromJS(UserPageViewModel_json);
-    
+
     // Get array of available exercise types as strings:
     self.user.exerciseTypes_names = ko.computed(function () {
-    var dataArray = new Array;
-    self.user.exerciseTypes().forEach(function(item){
-        dataArray.push(item.name());
-    }
-            
-                )
-return dataArray;
+        var dataArray = new Array;
+        self.user.exerciseTypes().forEach(function (item) {
+            dataArray.push(item.name());
+        }
+
+        )
+        return dataArray;
     }, self);
- 
+
 
     // console.log(self.user.events());
-    self.user.chosenExerciseType = ko.observableArray([self.user.exerciseTypes()[0]]);
+    self.user.chosenExerciseType = ko.observableArray([self.user.exerciseTypes_names()[0]]);
 
     // Find all exercise quantities:
     var quantities = [];
@@ -68,7 +68,28 @@ return dataArray;
             };
             // Extract events that have this exercise type:
             o.events = ko.utils.arrayFilter(self.user.events(), function (item, i) {
-                return stringStartsWith(item.type().toLowerCase(), exname);
+                return stringCompare(item.type(), exname);
+            });
+            a.push(o);
+        })
+        // dbg(a);
+        return a;
+    }, self);
+    
+    self.user.lastTotalExercises = ko.computed(function () {
+        // For each exercise type, find most recent date
+        a = [];
+        // Iterate over available exercise types:
+        // dbg(self.user.exerciseTypes()[0].name());
+        self.user.exerciseTypes().forEach(function (item, i) {
+            var exname = self.user.exerciseTypes()[i].name();
+            var o = {
+                'type': exname,
+                'events': []
+            };
+            // Extract events that have this exercise type:
+            o.events = ko.utils.arrayFilter(self.user.events(), function (item, i) {
+                return stringCompare(item.type(), exname);
             });
             a.push(o);
         })
@@ -77,22 +98,40 @@ return dataArray;
     }, self);
 
     self.addExercise = function () {
+        dbg(self.user.chosenExerciseType()[0]);
         var event = {
             'time': ko.observable(Date()),
             'type': ko.observable(self.user.chosenExerciseType()[0]),
-            'quantity': ko.observable(self.user.chosenQuantity()[0])
+            'quantity': ko.observable(Number(self.user.chosenQuantity()))
         }
-        dbg(self.user.events());
         self.user.events.push(event);
     }
+
+    self.removeExercise = function (ex) {
+        self.user.events.remove(ex);
+        dbg(ex);
+    }
+
 
 // Code related to creating new exercise definitions:
     self.newExerciseName = ko.observable("New amazing exercise");
     self.addExerciseDefinition = function () {
-        // Here is how new exercises are handled for now: For each new exercise
-        // an empty event is created, i.e. an event with quantity zero. Events 
-        // with zero quantity are not displayed in the exercise list
+        // Check if there already is the exact same exercise:
+        if (!arrayContains(self.user.exerciseTypes_names(), self.newExerciseName())) {
+            // Create new exercise type object:
+            var ety = {
+                'name': ko.observable(self.newExerciseName())
+            }
+            // Add new exercise to the list of exercises:
+            dbg(self.user.exerciseTypes());
+            self.user.exerciseTypes.push(ety)
+            dbg(self.user.exerciseTypes());
+        } else {
+            self.errorMessage("Exercise " + self.newExerciseName() + " already exists.");
+        }
     }
+
+    self.errorMessage = ko.observable("");
 
 }
 
@@ -104,11 +143,11 @@ function MainPageViewModel() {
     self.userPageVisible = ko.observable(true);
 }
 
-function stringStartsWith(string, startsWith) {
-    string = string || "";
-    if (startsWith.length > string.length)
-        return false;
-    return string.substring(0, startsWith.length) === startsWith;
+function stringCompare(s1, s2) {
+    if (s1.localeCompare(s2) === 0) {
+        return true;
+    }
+    return false;
 }
 
 function dbg(what) {
@@ -129,4 +168,8 @@ function upload() {
 
 function sortNumber(a, b) {
     return a - b;
+}
+
+function arrayContains(array, object) {
+    return $.inArray(object, array) > -1;
 }
